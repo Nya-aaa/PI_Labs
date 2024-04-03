72752bc10e4f1ac39408cd285fceff31b3e1e586
Nya-aaa <165551201+Nya-aaa@users.noreply.github.com>
2024-04-04 02:25:43 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+# TEMA 2
+
+# Задание 1
+# print(1 != 1)
+
+# Задание 2
+# a, b, c = 1, 2, 3
+# print(a, b, c)
+
+# Задание 3
+# num = int(input("Введите число: "))
+# print(num)
+
+# Задание 4
+# str = "word " * 5
+# print(len(str))
+
+# Задание 5
+# d, m, y = 4, "Апреля", 2024
+# print(f"Сегодня {d} {m} {y} года", end=". Всего хорошего!\n")
+
+# Задание 6
+# print("Hello" + " My " + "World")
+
+# Задание 7
+# print(len("Hello world"))
+
+# Задание 8
+# print("HELLO WORLD".lower())
+
+# Задание 9
+# a, b, c = 1, 2, 3
+# print(f"Наибольшее число среди {a}, {b} и {c} - {max(a, b, c)}")
+
+
+# Задание 10
+# a = "Hello "
+# b = "World"
+# print(f"Объединенная строка - {a + b}")
